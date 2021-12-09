2b9b33f9e16799316a43d4ab9265d2743644ebf7
Julian Baumgartner <jbaumgartner93@gmail.com>
2021-12-09 12:35:23 -0500

    change nameing convention

--- app/constants.py
+++ app/constants.py
@@ -4,9 +4,9 @@ from pathlib import Path
 FRAME_DATA_BUCKET = "webcam_data_scraping"
 LABELED_FRAME_DATA_BUCKET = "labeled_webcam_data"
 
-GLASSY_PREFIX = "surf_quality_data/glassy"
-SEMI_CHOP_PREFIX = "surf_quality_data/semi_chop"
-CHOPPY_PREFIX = "surf_quality_data/choppy"
+GLASSY_PREFIX = "surf_quality_data/good"
+SEMI_CHOP_PREFIX = "surf_quality_data/fair"
+CHOPPY_PREFIX = "surf_quality_data/poor"
 
 
 ACTIVE_PREFIX = "gating_data/visible"
