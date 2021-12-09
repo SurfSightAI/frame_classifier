8ceba815204e178057faaed8280ae38558a50f97
Julian Baumgartner <jbaumgartner93@gmail.com>
2021-12-09 14:19:36 -0500

    Use gcp copy to move images instead is PIL Images

--- app/utils.py
+++ app/utils.py
@@ -22,42 +22,31 @@ def get_frame(
     img_location: st.empty,
     error_location: st.empty,
     
-) -> Optional[Tuple[str, PIL_Image]]:
+) -> None:
     """
-    Return a genorator of all frames in bucket. Filter based on day, month and time.
+    Display first frame maching day, month and time filters.
+    Frame path and skipped frames managed in streamlit session_state (ephemeral browser session). 
     """
     storage_client = storage.Client()
     skipped_frames = st.session_state.get("skipped_frames")
     error_location.empty()
-
-    # Define labeled frames based on gating bucket or surf_quality bucket
-    if classification == "gating":
-        labeled_img_blobs_names = [
-            blob.name.split("/")[-1]
-            for index, blob in enumerate(
-                list(
-                    storage_client.list_blobs(
-                        constants.LABELED_FRAME_DATA_BUCKET,
-                        prefix=classification_map.get("Gating"),
-                    )
-                )
-            )
-            if blob.name.split("/")[-1].endswith(".png")
-        ]
-    if classification == "quality":
-        labeled_img_blobs_names = [
-            blob.name.split("/")[-1]
-            for index, blob in enumerate(
-                list(
-                    storage_client.list_blobs(
-                        constants.LABELED_FRAME_DATA_BUCKET,
-                        prefix=classification_map.get("Surf_Quality"),
-                    )
-                )
-            )
-            if blob.name.split("/")[-1].endswith(".png")
-        ]
-
+    
+    if st.session_state.get("undo") == True:
+        st.session_state["undo"] = False
+        frame_name = st.session_state.get("cur_frame_name")
+        frame_name = "/".join(frame_name.split("/")[2:])
+        
+        bucket = storage_client.bucket(constants.FRAME_DATA_BUCKET)
+        blob = bucket.blob(frame_name)
+        
+        data = blob.download_as_string()
+        img = Image.open(BytesIO(data))
+        img_location.image(img, caption=blob.name, width=1000)
+        _cache_frame(blob.name)
+        return
+        
+    labeled_img_blobs_names = _get_labeled_blob_names(storage_client, classification)
+    
     # All GCP storage blobs that end in .png and are from the selected spot
     img_blobs = [
         blob
@@ -87,7 +76,7 @@ def get_frame(
         data = blob.download_as_string()
         img = Image.open(BytesIO(data))
         img_location.image(img, caption=blob.name, width=1000)
-        _cache_frame(img, blob.name)
+        _cache_frame(blob.name)
 
         return blob.name, img
     
@@ -110,22 +99,23 @@ def get_spot_names() -> List[str]:
     )
 
 
-def _cache_frame(frame: PIL_Image, name: str) -> None:
+def _cache_frame(name: str) -> None:
     """Cache new frame"""
     st.session_state["cur_frame_name"] = name
-    st.session_state["cur_frame_data"] = frame.tobytes()
 
 
-def undo_previous_classification(image_location: st.empty, bucket: str, storage_client: storage.Client)-> None:
+def undo_previous_classification(image_location: st.empty, storage_client: storage.Client)-> None:
     prev_frame_path = st.session_state.get("prev_frame_name")
-    prev_frame_data = st.session_state.get("prev_frame_data")
-    if prev_frame_path and prev_frame_data:       
-        bucket = storage_client.bucket(bucket)
+    
+    if prev_frame_path:       
+        
+        bucket = storage_client.bucket(constants.LABELED_FRAME_DATA_BUCKET)
         blob = bucket.blob(prev_frame_path)
         blob.delete()
         
         st.session_state["cur_frame_name"] = prev_frame_path
-        st.session_state["cur_frame_data"] = prev_frame_data    
+        st.session_state["undo"] = True
+        del st.session_state["prev_frame_name"]
 
 
 def skip_frame(classification: str) -> None:
@@ -142,17 +132,61 @@ def skip_frame(classification: str) -> None:
 
 
 def save_frame(
-    bucket: str, frame_path: str, img_data: bytes, storage_client: storage.Client
+    frame_path: str, storage_client: storage.Client
 ) -> None:
     """Cache frame info to enable undo button. Save frame to GCP storage"""
     st.session_state["prev_frame_name"] = frame_path
-    st.session_state["prev_frame_data"] = img_data
     
-    bucket = storage_client.bucket(bucket)
-    blob = bucket.blob(frame_path)
-    blob.upload_from_string(img_data, content_type="image/png")
+    _copy_blob(storage_client, frame_path)
 
 
 def parse_to_date(frame_name) -> datetime.date:
     split_name = frame_name.split("_")
     return datetime.date(int(split_name[0]), int(split_name[1]), int(split_name[2]))
+
+
+def _copy_blob(
+    storage_client: storage.Client,
+    destination_blob_name: str,
+)-> None:
+    """Copy source frame to desired location"""
+    source_bucket = storage_client.bucket(constants.FRAME_DATA_BUCKET)
+    source_blob_name = st.session_state["cur_frame_name"]
+    
+    source_blob = source_bucket.blob(source_blob_name)
+    
+    destination_bucket = storage_client.bucket(constants.LABELED_FRAME_DATA_BUCKET)
+    
+    source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name
+    )
+
+
+def _get_labeled_blob_names(storage_client: storage.client, classification: str) -> List[str]:
+    """Define labeled frames based on gating bucket or surf_quality bucket"""
+    if classification == "gating":
+        return [
+            blob.name.split("/")[-1]
+            for index, blob in enumerate(
+                list(
+                    storage_client.list_blobs(
+                        constants.LABELED_FRAME_DATA_BUCKET,
+                        prefix=classification_map.get("Gating"),
+                    )
+                )
+            )
+            if blob.name.split("/")[-1].endswith(".png")
+        ]
+    if classification == "quality":
+        return [
+            blob.name.split("/")[-1]
+            for index, blob in enumerate(
+                list(
+                    storage_client.list_blobs(
+                        constants.LABELED_FRAME_DATA_BUCKET,
+                        prefix=classification_map.get("Surf_Quality"),
+                    )
+                )
+            )
+            if blob.name.split("/")[-1].endswith(".png")
+        ]
